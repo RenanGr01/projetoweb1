cd5d5b0aedc9f0c88590105b9d70be309c1a8242
RenanGr01 <renan.gregorio@edu.udesc.br>
2023-11-07 20:49:26 -0300

    Mudança de gênero

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -5,7 +5,7 @@ $idade = $_GET['idade'];
 
 echo '<h1> Dados do Usuário</h1>';
 echo '<hr>';
-echo "<p>O usuário $nome tem $idade anos.</p>";
+echo "<p>A usuária $nome tem $idade anos.</p>";
 
 echo '<a "href=http;//localhost/idusc/index.html"><button>Home Page</a>';
 echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
